a78b8c8b28702f62c12bc2a236d4c7cda624cfee
Allgod <murillo.tec@gmail.com>
2023-06-15 17:13:52 -0600

    Se inicio a implementar el movimiento de la ficha de jugador cuando se da click para tirar los dados

--- Tablero.py
+++ Tablero.py
@@ -1,6 +1,7 @@
 "El tablero del monopoly"
 
 import tkinter as tk
+import random
 
 # Create the main window
 window = tk.Tk()
@@ -25,7 +26,7 @@ properties = [
     {"name": "Connecticut\nAvenue", "color": "light blue", "x": 50, "y": 115},
     {"name": "Jail", "color": "grey", "x": 50, "y": 50},
     {"name": "St. Charles\nPlace", "color": "pink", "x": 115, "y": 50},
-    {"name": "Electric\nCompany", "color": "grey", "x": 180, "y": 50},
+    {"name": "Electric\nCompany", "color": "purple", "x": 180, "y": 50},
     {"name": "States\nAvenue", "color": "pink", "x": 245, "y": 50},
     {"name": "Virginia\nAvenue", "color": "pink", "x": 310, "y": 50},
     {"name": "St. James\nPlace", "color": "orange", "x": 440, "y": 50},
@@ -37,50 +38,98 @@ properties = [
     {"name": "Chance", "color": "magenta", "x": 700, "y": 180},
     {"name": "Indiana\nAvenue", "color": "red", "x": 700, "y": 245},
     {"name": "Illinois\nAvenue", "color": "red", "x": 700, "y": 310},
-    {"name": "Atlantic\nAvenue", "color": "yellow", "x": 700, "y": 375},
+    {"name": "Atlantic\nAvenue", "color": "yellow", "x": 700, "y": 440},
     {"name": "Ventnor\nAvenue", "color": "yellow", "x": 700, "y": 505},
-    {"name": "Water\nWorks", "color": "grey", "x": 700, "y": 570},
+    {"name": "Water\nWorks", "color": "purple", "x": 700, "y": 570},
     {"name": "Marvin\nGardens", "color": "yellow", "x": 700, "y": 635},
     {"name": "Go to Jail", "color": "grey", "x": 700, "y": 700},
-    {"name": "Pacific\nAvenue", "color": "green", "x": 500, "y": 765},
-    {"name": "North Carolina\nAvenue", "color": "green", "x": 450, "y": 700},
-    {"name": "Community\nChess", "color": "magenta", "x": 400, "y": 700},
-    {"name": "Pennsylvania\nAvenue", "color": "green", "x": 350, "y": 700},
-    {"name": "Chance", "color": "magenta", "x": 250, "y": 700},
-    {"name": "Park\nPlace", "color": "dark blue", "x": 200, "y": 700},
-    {"name": "Tax Over\nLuxury", "color": "grey", "x": 150, "y": 700},
-    {"name": "Boardwalk", "color": "dark blue", "x": 100, "y": 700},
+    {"name": "Pacific\nAvenue", "color": "green", "x": 635, "y": 700},
+    {"name": "North Carolina\nAvenue", "color": "green", "x": 570, "y": 700},
+    {"name": "Community\nChess", "color": "magenta", "x": 505, "y": 700},
+    {"name": "Pennsylvania\nAvenue", "color": "green", "x": 440, "y": 700},
+    {"name": "Chance", "color": "magenta", "x": 310, "y": 700},
+    {"name": "Park\nPlace", "color": "dark blue", "x": 245, "y": 700},
+    {"name": "Tax Over\nLuxury", "color": "grey", "x": 180, "y": 700},
+    {"name": "Boardwalk", "color": "dark blue", "x": 115, "y": 700},
 ]
 
 # Define the railroads and their coordinates
 railroads = [
-    {"name": "Reading\nRailroad", "color": "black", "x": 50, "y": 375},
-    {"name": "Pennsylvania\nRailroad", "color": "black", "x": 375, "y": 50},
-    {"name": "B. & O.\nRailroad", "color": "black", "x": 700, "y": 440},
-    {"name": "Short\nLine", "color": "black", "x": 300, "y": 700},
+    {"name": "Reading\nRailroad", "color": "light grey", "x": 50, "y": 375},
+    {"name": "Pennsylvania\nRailroad", "color": "light grey", "x": 375, "y": 50},
+    {"name": "B. & O.\nRailroad", "color": "light grey", "x": 700, "y": 375},
+    {"name": "Short\nLine", "color": "light grey", "x": 375, "y": 700},
 ]
+
+# Load dice images
+dice_images = [
+    tk.PhotoImage(file="Imagenes\dice1.png"),
+    tk.PhotoImage(file="Imagenes\dice2.png"),
+    tk.PhotoImage(file="Imagenes\dice3.png"),
+    tk.PhotoImage(file="Imagenes\dice4.png"),
+    tk.PhotoImage(file="Imagenes\dice5.png"),
+    tk.PhotoImage(file="Imagenes\dice6.png"),
+]
+
+player_token_image = tk.PhotoImage(file="Imagenes/token1.png")
 
 # Draw the properties on the game board
 square_width = 65
+text_font = ("Arial", 8)
 
 for prop in properties:
     x = prop["x"]
     y = prop["y"]
     color = prop["color"]
-
     canvas.create_rectangle(x, y, x + square_width, y + square_width, fill=color)
+    canvas.create_text(x + square_width / 2, y + square_width / 2, text=prop["name"], font=text_font, anchor="center")
 
 for rr in railroads:
     x = rr["x"]
     y = rr["y"]
     color = rr["color"]
-
     canvas.create_rectangle(x, y, x + square_width, y + square_width, fill=color)
+    canvas.create_text(x + square_width / 2, y + square_width / 2, text=rr["name"], font=text_font, anchor="center")
 
-# Draw other game elements such as railroads and utilities
-# You can define their coordinates and use appropriate shapes or icons
+# Function to roll the dice and show the result
+def roll_dice():
+    # Simulate rolling the dice and getting the results
+    dice_result1 = random.randint(1, 6)
+    dice_result2 = random.randint(1, 6)
+    canvas.delete("dice")
+    canvas.create_image(820, 150, image=dice_images[dice_result1 - 1], tags="dice")
+    canvas.create_image(880, 150, image=dice_images[dice_result2 - 1], tags="dice")
+    move_player_token(dice_result1 + dice_result2)
 
-# Draw other game board elements (e.g., railroads, utilities, special spaces)
+# Function to move the player token
+def move_player_token(steps):
+    # Get the current position of the player token
+    x, y = canvas.coords(player_token)
+
+    # Calculate the new position after moving the given number of steps
+    new_x = x + steps * square_width
+
+    # Check if the new position is beyond the board boundaries
+    if new_x >= 1920:
+        # Player token reached or passed the end of the board, move it back to the start
+        new_x = player_start_x
+        y = player_start_y
+    elif new_x >= 700 and y == player_start_y:
+        # Player token reached the end row, move it to the next row
+        new_x = 700
+        y -= square_width
+
+    # Move the player token to the new position
+    canvas.coords(player_token, new_x, y)
+
+roll_button = tk.Button(canvas, text="Roll Dice", command=roll_dice)
+canvas.create_window(820, 100, window=roll_button)
+
+# Create the player token on the board
+player_start_x = 80  # Initial X position of the player token
+player_start_y = 730  # Initial Y position of the player token
+player_token = canvas.create_image(player_start_x, player_start_y, image=player_token_image)
+
 
 # Start the game loop
 window.mainloop()
